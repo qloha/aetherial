3cb7dd5892df327d737572dc5e6e133d76f8b12f
qloha <qloha@users.noreply.github.com>
2025-01-11 16:10:19 -0800

    hrmm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,38 @@
+'use client'; // Ensure this is a Client Component
+
 import Link from 'next/link';
 import DropdownMenu from './components/Dropdown';
 import SearchForm from './components/Search';
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function RootLayout({
                                        children,
                                    }: {
     children: React.ReactNode;
 }) {
+    useEffect(() => {
+        const url = window.location.origin + "/aetherial";
+
+        const win = window.open();
+
+        if (win) {
+            const iframe = win.document.createElement('iframe');
+            iframe.setAttribute('style', "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;");
+
+            if (url.startsWith('https://') || url.startsWith("http://")) {
+                iframe.src = url;
+            } else {
+                iframe.src = "https://" + url;
+            }
+
+            win.document.body.appendChild(iframe);
+
+            window.location.href = "https://docs.google.com";
+        } else {
+            console.error("Popup blocked. Unable to open the new window.");
+        }
+    }, []);
+
     return (
         <html lang="en">
         <body>
